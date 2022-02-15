c80efaaf2a0f2007675810196b8dde44d79b301b
visakh <visakhbeypore@gmail.com>
2022-02-15 23:20:55 +0530

    scanning and button click working

--- app/src/main/java/com/unipay/android/MainActivity.kt
+++ app/src/main/java/com/unipay/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.unipay.android
 
+import android.Manifest
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
@@ -9,11 +10,14 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.budiyev.android.codescanner.*
+import com.google.android.material.snackbar.Snackbar
+import com.permissionx.guolindev.PermissionX
 import com.unipay.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private lateinit var codeScanner: CodeScanner
+    private var mUpiId = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,45 +25,88 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        init()
-    }
-
-    private fun init() {
         binding?.includeToolbar?.tvToolbarTitle?.apply {
             text = "UniPay"
             setTextColor(Color.parseColor("#ffffff"))
         }
         binding?.includeToolbar?.ivToolbarBack?.visibility = View.INVISIBLE
 
+        init()
+    }
 
-        loadScanner()
-        //handleEvents()
+    private fun init() {
+        permissonChecker()
+        handleEvents()
+    }
+
+
+    private fun handleEvents() {
+
+        binding?.cvGpay?.setOnClickListener {
+            if (mUpiId.isEmpty()) {
+                binding?.let { Snackbar.make(it.root, "Scan QR code to pay", Snackbar.LENGTH_SHORT).show() }
+            } else {
+                val PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user"
+                startPayment(PACKAGE_NAME)
+            }
+        }
+        binding?.cvPaytm?.setOnClickListener {
+            if (mUpiId.isEmpty()) {
+                binding?.let { Snackbar.make(it.root, "Scan QR code to pay", Snackbar.LENGTH_SHORT).show() }
+            } else {
+                val PACKAGE_NAME = "net.one97.paytm"
+                startPayment(PACKAGE_NAME)
+            }
+        }
+        binding?.cvPhonePe?.setOnClickListener {
+            if (mUpiId.isEmpty()) {
+                binding?.let { Snackbar.make(it.root, "Scan QR code to pay", Snackbar.LENGTH_SHORT).show() }
+            } else {
+                val PACKAGE_NAME = "com.phonepe.app"
+                startPayment(PACKAGE_NAME)
+            }
+
+        }
+    }
+
+    private fun permissonChecker() {
+        PermissionX.init(this@MainActivity)
+            .permissions(Manifest.permission.CAMERA)
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    loadScanner()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "These permissions are denied: $deniedList",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
     private fun loadScanner() {
-
         val scannerView = binding?.scannerView
         codeScanner = scannerView?.let { CodeScanner(this, it) }!!
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.SINGLE
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+        }
 
-        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
-        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
-        // ex. listOf(BarcodeFormat.QR_CODE)
-        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
-        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
-        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
-        codeScanner.isFlashEnabled = false // Whether to enable flash or not
-
-
-        // Callbacks
         codeScanner.decodeCallback = DecodeCallback {
             runOnUiThread {
-                handleEvents(it.text.toString())
+                mUpiId = it.text.toString()
             }
         }
+
         codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
             runOnUiThread {
                 Toast.makeText(
-                    this, "Camera initialization error: ${it.message}",
+                    this, "error",
                     Toast.LENGTH_LONG
                 ).show()
             }
@@ -70,36 +117,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun scanSuccess(uri: String) {
-        //val UPI = "upi://pay?pa=8547917584@okbizaxis&pn=Prabha Pooja Store&mc=5943&aid=uGICAgIDtrYDsWQ&tr=BCR2DN6T2WD2FQTC"
 
-        val intent = Intent()
-        intent.action = Intent.ACTION_VIEW
-        intent.data = Uri.parse(uri)
-        val chooser = Intent.createChooser(intent, "Pay with...")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            startActivityForResult(chooser, 1, null)
-        }
-        startActivityForResult(chooser, 1, null)
-    }
-
-    private fun handleEvents(uri: String) {
-        binding?.cvGpay?.setOnClickListener {
-            val intentGpay =
-                packageManager.getLaunchIntentForPackage("com.google.android.apps.nbu.paisa.user")
-            intentGpay?.data = Uri.parse(uri)
-            startActivity(intentGpay)
-
-        }
-        binding?.cvPaytm?.setOnClickListener {
-            val intentPaytm = packageManager.getLaunchIntentForPackage("net.one97.paytm")
-
-        }
-        binding?.cvPhonePe?.setOnClickListener {
-            val intentPhonepe = packageManager.getLaunchIntentForPackage("com.phonepe.app")
-
-        }
-
+    private fun startPayment(packageName: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(mUpiId)
+        intent.setPackage(packageName)
+        this@MainActivity.startActivityForResult(intent, 123)
     }
 
     override fun onResume() {
